bf85c500e9758fd54652b1184bdba4223ba1d280
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-04 15:17:18 -0500

    create library to declare entities

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Entities;
 
 namespace ConsoleApp
 {
@@ -10,6 +11,9 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Search s = new Search();
+            Console.WriteLine(s.getData());
+            Console.ReadLine();
         }
     }
 }
