0ab3a3838afb34cc5bcc7660d283b3ecb3f6af89
Ruben Paz <rubenpazchuspe@outlook.com>
2020-10-08 20:11:35 -0500

    add initial interface and entity to search a term

---
+++ searchfight/Models/ISearchFight.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Models
+{
+    public interface ISearchFight
+    {
+        TermSearchResult[] GetTermSearchResults(string[] terms);
+        bool TryValidateTerms(string[] terms, string[] validateErrors);
+    }
+}
---
+++ searchfight/Models/TermSearchResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    public class TermSearchResult
+    {
+        public long Count { get; set; }
+        public string SearchEngineName { get; set; }
+        public string Term { get; set; }
+    }
+}
